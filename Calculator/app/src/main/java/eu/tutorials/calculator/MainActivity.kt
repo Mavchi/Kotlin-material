4815a37d75dd2e77842bdd04e5e16561f1b86922
Aleksander <aleksi.khlebnikov@gmail.com>
2023-03-02 18:39:02 +0200

    simple calc ability, two decimals not supported yet or several operators. need to add later on for practise

--- Calculator/app/src/main/java/eu/tutorials/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/eu/tutorials/calculator/MainActivity.kt
@@ -48,6 +48,73 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun onEqual(view: View) {
+        if(lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            try {
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }
+                if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+                if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }
+            } catch(e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String) : String {
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0, result.length - 2)
+
+        return value
+    }
+
     private fun isOperatorAdded(value: String): Boolean {
         return if(value.startsWith("-")){
             false
